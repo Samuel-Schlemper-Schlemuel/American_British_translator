30c7f2a67be6a15294f7f1dae23a37e62279cbe9
Samuel Schlemper <samuelschlemper2006@gmail.com>
2023-12-19 21:40:20 -0300

    Finishing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,85 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    test('Basic function', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'text': 'Mangoes are my favorite fruit.',
+         'locale': 'american-to-british'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, {text: 'Mangoes are my favorite fruit.', translation: 'Mangoes are my <span class="highlight">favourite</span> fruit.'})
+          done()
+        })
+    })
 
+    test('Invalid locale', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'text': 'Anything',
+         'locale': 'Invalid'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, { error: 'Invalid value for locale field' })
+          done()
+        })
+    })
+
+    test('Undefined text', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'locale': 'Anything'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, { error: 'Required field(s) missing' })
+          done()
+        })
+    })
+
+    test('Undefined locale', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'text': 'Anything'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, { error: 'Required field(s) missing' })
+          done()
+        })
+    })
+
+    test('Undefined locale', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'text': '',
+         'locale': 'american-to-british'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, { error: 'No text to translate' })
+          done()
+        })
+    })
+
+    test('The translation is not necessary', function(done) {
+        chai.request(server)
+        .post('/api/translate')
+        .send({
+         'text': 'Anything',
+         'locale': 'american-to-british'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.deepEqual(res.body, {text: 'Anything', translation: 'Everything looks good to me!' })
+          done()
+        })
+    })
 });
